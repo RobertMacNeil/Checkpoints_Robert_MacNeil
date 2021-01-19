8ab9d958216f5e85f8f94f9492458e0c7ec1d62f
frozentiger279 <55792108+frozentiger279@users.noreply.github.com>
2021-01-19 09:38:37 -0400

    Initial commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+    public Text Checkpoint;
+    Vector3 RepawnPoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Checkpoint.text = " ";
+        RepawnPoint = this.transform.position;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "KillBox")
+        {
+            this.gameObject.transform.position = RepawnPoint;
+        }
+
+        if (other.tag == "Checkpoint")
+        {
+            RepawnPoint = this.transform.position;
+            Checkpoint.text = "Checkpoint!";
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Checkpoint")
+        {
+            Checkpoint.text = " ";
+        }
+    }
+}
